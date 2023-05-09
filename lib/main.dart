09102187c1042ae5a567d65af16e689045704feb
Matt Kaufman <the1mattkaufman@gmail.com>
2023-05-08 22:02:59 -0700

    built for web

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super(key: key});
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -34,205 +34,101 @@ class MyKaardApp extends StatefulWidget {
 
 class _MyKaardAppState extends State<MyKaardApp> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(),
-        child: Stack(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(),
+            child: Stack(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: Center(
-                    child: Column(
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: Center(
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                              CircleAvatar(
+                                radius: 100,
+                                backgroundImage: NetworkImage(
+                                    'https://avatars.githubusercontent.com/u/15354688?v=4'),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text('Matt Kaufman',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.teal,
+                                    fontSize: 28,
+                                  )),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text('Business Leader',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 20,
+                                  ))
+                            ]))),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Center(
+                        child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        CircleAvatar(
-                          radius: 100,
-                          backgroundImage: NetworkImage(
-                            'https://avatars.githubusercontent.com/u/15354688?v=4'
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          'Matt Kaufman',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.teal,
-                            fontSize: 28,
-                          )
-                        ),
-                        SizedBox(
+                      children: [
+                        Container(
+                            height: 60,
+                            width: 200,
+                            decoration: BoxDecoration(
+                              color: Colors.tealAccent,
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: SocialWidget(
+                                  placeholderText: 'the1mattkaufman',
+                                  iconData: SocialIconsFlutter.github,
+                                  iconColor: Colors.black,
+                                  iconSize: 24,
+                                  link: 'https://github.com/the1mattkaufman',
+                                  placeholderStyle: const TextStyle(
+                                      color: Colors.black, fontSize: 18),
+                                ))),
+                        const SizedBox(
                           height: 5,
                         ),
-                        Text(
-                          'Business Leader',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontStyle: FontStyle.italic,
-                            fontSize: 20,
-                          )
-                        )
-                      ]
-                    )
-                  )
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 60,
-                        width: 150,
-                        decoration: BoxDecoration(
-                          color: Colors.tealAccent,
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: SocialWidget(
-                            placeholderText: 'the1mattkaufman',
-                            iconData: SocialIconsFlutter.github,
-                            iconColor: Colors.black,
-                            iconSize: 24,
-                            link: 'https://github.com/the1mattkaufman',
-                            placeholderStyle: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 18
+                        Container(
+                            height: 60,
+                            width: 200,
+                            decoration: BoxDecoration(
+                              color: Colors.tealAccent,
+                              borderRadius: BorderRadius.circular(15),
                             ),
-                          )
-                        ) 
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Container(
-                        height: 60,
-                        width: 150,
-                        decoration: BoxDecoration(
-                          color: Colors.tealAccent,
-                          borderRadius: BorderRadius.circular(15),
+                            child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: SocialWidget(
+                                  placeholderText: 'the1mattkaufman',
+                                  iconData: SocialIconsFlutter.twitter,
+                                  iconColor: Colors.blue,
+                                  iconSize: 24,
+                                  link: 'https://twitter.com/the1mattkaufman',
+                                  placeholderStyle: const TextStyle(
+                                      color: Colors.black, fontSize: 18),
+                                ))),
+                        const SizedBox(
+                          height: 5,
                         ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: SocialWidget(
-                            placeholderText: 'the1mattkaufman',
-                            iconData: SocialIconsFlutter.twitter,
-                            iconColor: Colors.blue,
-                            iconSize: 24,
-                            link: 'https://twitter.com/the1mattkaufman',
-                            placeholderStyle: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 18
-                            ),
-                          )
-                        ) 
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                    ],
-                  )
+                      ],
+                    ))
+                  ],
                 )
               ],
-            )
-          ],
-        )
-        )
-    )
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            )));
   }
 }
