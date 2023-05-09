3e462501836f28d5e0a592cb6b0f59b515d9d2d4
Matt Kaufman <the1mattkaufman@gmail.com>
2023-05-08 21:23:39 -0700

    added github social

--- lib/main.dart
+++ lib/main.dart
@@ -90,6 +90,38 @@ class _MyKaardAppState extends State<MyKaardApp> {
                 ),
                 const SizedBox(
                   height: 5,
+                ),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 60,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          color: Colors.tealAccent,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10,0),
+                          child: SocialWidget(
+                            placeholderText: 'the1mattkaufman',
+                            iconData: SocialIconsFlutter.github,
+                            iconColor: Colors.black,
+                            iconSize: 24,
+                            link: 'https://github.com/the1mattkaufman',
+                            placeholderStyle: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 18
+                            ),
+                          )
+                        ) 
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  )
                 )
               ],
             )
